1cb91b312eaaaf224fcaf090dff75daa55abb829
romanlp <roman.lapacherie@gmail.com>
2020-12-28 17:30:31 +0000

    remove google font

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,12 +26,7 @@ export default {
       }
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "stylesheet",
-        href:
-          "https://fonts.googleapis.com/css?family=Alata|Open+Sans&display=swap"
-      }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
     ]
   },
   /*
