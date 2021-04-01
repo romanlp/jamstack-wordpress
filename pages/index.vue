70682ae7c0b4cd0d1fba9c1b0cbcb0430b78a330
romanlp <roman.lapacherie@gmail.com>
2021-04-01 10:37:37 +0100

    add web vitals mesures

--- pages/index.vue
+++ pages/index.vue
@@ -34,6 +34,34 @@
 
 <script>
 import AppMasthead from "@/components/AppMasthead.vue";
+import {getLCP, getFID, getCLS} from 'web-vitals';
+
+if (process.client) {
+  
+  function sendToGoogleAnalytics({name, delta, id}) {
+    // Assumes the global `gtag()` function exists, see:
+    // https://developers.google.com/analytics/devguides/collection/gtagjs
+    gtag('event', name, {
+      event_category: 'Web Vitals',
+      // Google Analytics metrics must be integers, so the value is rounded.
+      // For CLS the value is first multiplied by 1000 for greater precision
+      // (note: increase the multiplier for greater precision if needed).
+      value: Math.round(name === 'CLS' ? delta * 1000 : delta),
+      // The `id` value will be unique to the current page load. When sending
+      // multiple values from the same page (e.g. for CLS), Google Analytics can
+      // compute a total by grouping on this ID (note: requires `eventLabel` to
+      // be a dimension in your report).
+      event_label: id,
+      // Use a non-interaction event to avoid affecting bounce rate.
+      non_interaction: true,
+    });
+  }
+
+  getCLS(sendToGoogleAnalytics);
+  getFID(sendToGoogleAnalytics);
+  getLCP(sendToGoogleAnalytics);
+
+}
 
 export default {
   components: {
